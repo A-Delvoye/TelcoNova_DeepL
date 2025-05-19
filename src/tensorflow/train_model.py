277625d50dc779803c7913ead086218b8893fdd8
istaion <v.poutot@gmail.com>
2025-05-19 16:39:34 +0200

    :sparkles: first script

---
+++ src/tensorflow/train_model.py
@@ -0,0 +1,124 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import train_test_split
+import tensorflow as tf
+import os
+
+def make_preprocess_pipeline(numerical_features, categorical_features):
+    numerical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='mean')),
+        ('scaler', StandardScaler())
+    ])
+
+    categorical_transformer = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('onehot', OneHotEncoder(handle_unknown='ignore'))
+    ])
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ('num', numerical_transformer, numerical_features),
+            ('cat', categorical_transformer, categorical_features)
+        ],
+        remainder='passthrough'
+    )
+
+    return preprocessor
+
+def preprocess_data(df):
+    feature_of_interest = [
+        'gender', 'SeniorCitizen', 'Partner', 'Dependents',
+        'tenure', 'PhoneService', 'MultipleLines', 'InternetService',
+        'OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
+        'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
+        'PaymentMethod', 'MonthlyCharges', 'TotalCharges'
+    ]
+
+    target = 'Churn'
+
+    categorical_features = [
+        "DeviceProtection",
+        "MultipleLines",	
+        "InternetService",	
+        "OnlineSecurity",
+        "OnlineBackup",
+        'TechSupport',
+        'StreamingTV',
+        'StreamingMovies',
+        'Contract',
+        'PaymentMethod'
+    ]
+
+    numerical_features = [
+        'gender', 'SeniorCitizen', 'Partner', 'Dependents',
+        'tenure', 'PhoneService', 'PaperlessBilling',
+        'MonthlyCharges', 'TotalCharges'
+    ]
+
+    df["gender"]= df["gender"].apply(lambda x: 0 if x ==  'Male' else 1)
+    for col in ["Partner", "PhoneService", 'PaperlessBilling', "Dependents", 'Churn']:
+        df[col]= df[col].apply(lambda x: 0 if x ==  'No' else 1)
+    for col in numerical_features:
+        df[col] = pd.to_numeric(df[col], errors="coerce")
+    df.set_index("customerID", inplace=True)
+
+    X = df.drop(columns=[target])
+    y = df[target]
+
+    # Séparation train/val/test (80/20 puis 20% de train pour val)
+    X_train_0, X_test, y_train_0, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
+
+    # On prend 20% de X_train pour validation
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_0, y_train_0, test_size=0.2, random_state=42, stratify=y_train_0
+    )
+
+    preprocessor = make_preprocess_pipeline(numerical_features, categorical_features)
+
+    X_train = preprocessor.fit_transform(X_train)
+    X_val  = preprocessor.transform(X_val)
+    X_test  = preprocessor.transform(X_test)
+
+    return df, X_train, X_val, X_test, y_train, y_val, y_test 
+
+
+def build_model(X_train):
+    # Réseau avec 2 couches cachées de 64 neurones chacune
+    # et une couche de sortie avec activation softmax pour classification
+    model = tf.keras.Sequential([
+        tf.keras.layers.Input(shape=(X_train.shape[1],)),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(1, activation='sigmoid')
+    ])
+
+    # Définition de la fonction de perte, de l'optimiseur et des métriques
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+
+    model.summary()
+
+    return model
+
+if __name__ == "__main__":
+    df = pd.read_csv("data/WA_Fn-UseC_-Telco-Customer-Churn.csv")
+    os.environ['CUDA_VISIBLE_DEVICES'] = 'GPU:0'
+    print("GPUs disponibles :", tf.config.list_physical_devices('GPU'))
+    df, X_train, X_val, X_test, y_train, y_val, y_test = preprocess_data(df)
+    model = build_model(X_train)
+    history = model.fit(
+        X_train, y_train,
+        validation_data=(X_val, y_val),
+        epochs=100,
+        batch_size=16,
+        verbose=1
+    )
